ae7ec1573f09a2c55590070e8ed147fd131dceb8
daddyincase168 <somonorhong011@gmail.com>
2025-06-16 23:43:29 +0700

    route woildcards and view data

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,16 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Ninja Network | Home</title>
 </head>
+
 <body>
     <h2>Currently Available Ninjas</h2>
-
+    <p>{{ $greeting }}</p>
     <ul>
-        <li>Ninjas Here</li>
+        <li>
+            <a href="ninjas/{{ $ninjas[0]['id'] }}">{{ $ninjas[0]['name'] }}</a>
+        </li>
+        <li>
+            <a href="ninjas/{{ $ninjas[1]['id'] }}">{{ $ninjas[1]['name'] }}</a>
+        </li>
     </ul>
 </body>
+
 </html>
