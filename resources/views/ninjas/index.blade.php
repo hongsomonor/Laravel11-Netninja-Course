ab2a6b636b22d775e8b7c1d2e347ef84f8cc23fc
daddyincase168 <somonorhong011@gmail.com>
2025-06-17 00:01:08 +0700

    layout and slot

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,23 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ninja Network | Home</title>
-</head>
-
-<body>
+<x-layout>
     <h2>Currently Available Ninjas</h2>
-    @if ($greeting == "Hello")
+    @if ($greeting == 'Hello')
         <p>Hi from inside if statement</p>
     @endif
     <ul>
         @foreach ($ninjas as $ninja)
             <li>
-                <p>{{ $ninja["name"] }}</p>
-                <a href="ninjas/{{ $ninja["id"] }}">View detail</a>
+                <p>{{ $ninja['name'] }}</p>
+                <a href="ninjas/{{ $ninja['id'] }}">View detail</a>
             </li>
         @endforeach
         {{-- <li>
@@ -27,6 +17,4 @@
             <a href="ninjas/{{ $ninjas[1]['id'] }}">{{ $ninjas[1]['name'] }}</a>
         </li> --}}
     </ul>
-</body>
-
-</html>
+</x-layout>
