a05b276e37f5c34782762e988ae9e515deabaf9e
daddyincase168 <somonorhong011@gmail.com>
2025-06-16 23:49:23 +0700

    blade directive

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -10,14 +10,22 @@
 
 <body>
     <h2>Currently Available Ninjas</h2>
-    <p>{{ $greeting }}</p>
+    @if ($greeting == "Hello")
+        <p>Hi from inside if statement</p>
+    @endif
     <ul>
-        <li>
+        @foreach ($ninjas as $ninja)
+            <li>
+                <p>{{ $ninja["name"] }}</p>
+                <a href="ninjas/{{ $ninja["id"] }}">View detail</a>
+            </li>
+        @endforeach
+        {{-- <li>
             <a href="ninjas/{{ $ninjas[0]['id'] }}">{{ $ninjas[0]['name'] }}</a>
         </li>
         <li>
             <a href="ninjas/{{ $ninjas[1]['id'] }}">{{ $ninjas[1]['name'] }}</a>
-        </li>
+        </li> --}}
     </ul>
 </body>
 
